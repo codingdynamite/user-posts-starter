4cb5ae171dbd82e023f04c6cb33f8cf7b3f6c586
Joshua Mason <thelementalcode@gmail.com>
2025-05-02 09:43:13 -0700

    first push with initial problem

---
+++ index.js
@@ -0,0 +1,10 @@
+// API 1: "https://jsonplaceholder.typicode.com/users"
+// API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+async function main() {
+    const users = await fetch("https://jsonplaceholder.typicode.com/users");
+    const userData = await users.json();
+    console.log(usersData);
+}
+
+main();
